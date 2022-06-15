872e9fa6f3ac511355d4617e7084a70ca758b1b4
Mahesh Bauri <www.maheshbauri@gmail.com>
2022-06-15 10:34:39 +0530

    lesson-5 Navigation Demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./components/category-item/category-item.style.scss";
 import Home from "./components/routes/home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Outlet, Route } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am Navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1>I am Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
