e60b7b2e44fb53c7095efbfda78df78d7e242626
Mahesh Bauri <www.maheshbauri@gmail.com>
2022-07-04 08:42:09 +0530

    project compeleted

--- src/components/directory-item/DirectoryItem.js
+++ src/components/directory-item/DirectoryItem.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   DirectoryItemContainer,
   BackgroundImage,
   Body,
 } from "./directory-item.styles";
 function DirectoryItem({ category }) {
-  const { title, imageUrl } = category;
+  const { title, imageUrl, route } = category;
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
